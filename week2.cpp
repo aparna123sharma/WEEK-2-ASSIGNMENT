841e1d5836d1a2ecb68dc827819f6c180bab2b27
aparna123sharma <152768279+aparna123sharma@users.noreply.github.com>
2023-12-03 12:47:09 +0530

    Add files via upload

---
+++ week2.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+//	Q. 1 Write a program to interchange two values by using Assignment Operator.
+//int a,b;
+//printf("Please enter the value of two numbers: ");
+//scanf("%d %d",&a,&b);
+//printf("a=%d and b=%d is the value of number before interchanaging",a,b);
+//a = a + b;
+//b = a - b;
+//a = a - b;
+//printf("\na=%d and b=%d is the value of number after interchanging",a,b);
+//return 0;
+
+//Q. 2 Write a program to interchange two values by using Arithmetic Operator.
+//int a,b,e;
+//printf("Please enter the value of two numbers: ");
+//scanf("%d %d",&a,&b);
+//printf("a=%d and b=%d is the value of number before interchanaging",a,b);
+//e=a ;
+//a=b;
+//b=e;
+//printf("\na=%d and b=%d is the value of number after interchanging",a,b);
+//return 0;
+
+
+//Q. 3 Write a program to interchange two values by using bitwise Operator.
+//int a,b,t;
+//printf("enter the value of the numbers:");
+//scanf("%d %d",&a,&b);
+//printf("a=%d and b=%d is the value of number before interchanaging",a,b);
+//a=a^b;
+//b=a^b;
+//a=a^b;
+//printf("\na=%d and b=%d is the value of number after interchanging",a,b);
+//return 0;
+
+
+//Q. 4 Write a program to find the size of all data types (Int, Float, Char, Double, 
+//Long Double, Short Int etc.).
+//printf("int size=%lu\n",sizeof(int));
+//printf("float size=%lu\n",sizeof(float));
+//printf("char size=%lu\n",sizeof(char));
+//printf("double size=%lu\n",sizeof(double));
+//printf("long double size=%lu\n",sizeof(long double));
+//printf("short int size=%lu\n",sizeof(short int));
+//return 0;
+
+
+//Q. 5 Write a program to find out whether input number is even or odd without using 
+//arithmetic operators.
+//int a;
+//printf("Please enter the value of number:");
+//scanf("%d",&a);
+//if((a&1)==0)
+//{
+//	printf("even");
+//}
+//else
+//{
+//	printf("odd");
+//}
+//return 0;
+//}
